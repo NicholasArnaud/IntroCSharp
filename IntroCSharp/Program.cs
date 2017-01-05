da5ce80bbfaded0490bbbe591e0c8f0b6ab6f751
NicholasArnaud <cobrahead09@gmail.com>
2017-01-04 18:01:35 -0600

    StartUp

---
+++ IntroCSharp/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class Practice
+            Enemy Joe = new Enemy();
+            Joe.Attack();
+            Console.WriteLine(Joe.Health);
+            Console.WriteLine(Joe.Name);
+
+
+            //Maths
+            int lo = 4;
+            int jo = lo + 3;
+
+
+
+
+            //Linked List Practice
+            List<Student> students = new List<Student>();
+            Student Bob = new Student(10);
+            students.Add(Bob);
+            Student s = new Student(9);
+
+
+            //String Practice
+            string add = "Bob";
+            string addie = "Joe";
+            add += addie;
+            Console.WriteLine(add);
+            
+            //Printing
+            Console.WriteLine("Hello World");
+            Console.WriteLine(jo);
+            Console.Read();
+        }
+    }
+
+    class Student
+    {
+        public Student() { }
+        public Student(int ID){ id = ID; }
+        
+        private int id;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+    }
+
+
+
+    class Enemy
+    {
+        public Enemy()
+        {
+            m_health = 100;
+            m_attack = 20;
+            Name = "Assassin";
+        }
+
+        private int m_health;
+        private int m_attack;
+        private string m_name;
+        public void Attack()
+        {
+
+            Console.WriteLine("Attacked :", Name);
+            m_health -= m_attack;
+
+        }
+
+
+        public int Health
+        {
+            get
+            {
+                return m_health;
+            }
+            set
+            {
+                m_health = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+
+            set
+            {
+                m_name = value;
+            }
+        }
+    }
+}
+
---
+++ NinjasVsZombies/NinjasVsZombies/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjasVsZombies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 5;
+            Player p = new Player(1,1);
+            Console.WriteLine(p.name);
+            while (true)
+            {
+               
+                Console.ReadLine();
+                num++;
+            }
+
+        }
+    }
+
+    class Entity
+    {
+        public Entity() { }
+        public Entity(int h, int att)
+        {
+            health = h;
+            attackPower = att;
+        }
+        private int health;
+        protected int Health
+        {
+            get { return health = Health; }
+            set { health = value; }
+        }
+        private int attackPower;
+        protected int AttackPower
+        {
+            get { return attackPower = AttackPower; }
+            set { attackPower = value; }
+        }
+        public virtual bool Attack(Entity enemy) { return true; }
+        private string Name
+        {
+            get { return name = Name; }
+            set { name = value; }
+        }
+
+        public string name;
+    }
+
+    class Zombie : Entity
+    {
+        public Zombie() { }
+        public Zombie(int h, int att) { }
+        public override bool Attack(Entity enemy) { return true; }
+    }
+
+    class Player : Entity
+    {
+        public Player() { }
+        public Player(int h, int att) : base(h,att)
+        {
+            
+        }
+        public override bool Attack(Entity enemy) { return true; }
+        public bool Defend() { return true; }
+    }
+
+}
