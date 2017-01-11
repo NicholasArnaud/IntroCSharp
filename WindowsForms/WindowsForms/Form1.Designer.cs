d08b7f587c16dda3b2d006f3850c73799c9f8541
NicholasArnaud <cobrahead09@gmail.com>
2017-01-11 17:56:03 -0600

    WinForm Exercise

---
+++ WindowsForms/WindowsForms/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsForms
+{
+    partial class Leveler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textLvl = new System.Windows.Forms.TextBox();
+            this.textExp = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gimmie Gimmie Chicken Tendies";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Progress);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(71, 106);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(120, 37);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 82);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Take The Chicken Tendies Back!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textLvl.Location = new System.Drawing.Point(81, 44);
+            this.textLvl.Name = "textBox1";
+            this.textLvl.ReadOnly = true;
+            this.textLvl.Size = new System.Drawing.Size(100, 20);
+            this.textLvl.TabIndex = 5;
+            this.textLvl.TextChanged += new System.EventHandler(this.textlvl_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textExp.Location = new System.Drawing.Point(81, 70);
+            this.textExp.Name = "textBox2";
+            this.textExp.ReadOnly = true;
+            this.textExp.Size = new System.Drawing.Size(100, 20);
+            this.textExp.TabIndex = 6;
+            this.textExp.TextChanged += new System.EventHandler(this.textexp_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(627, 202);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Found Secret";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.SecretCheck_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Leveler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 317);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textExp);
+            this.Controls.Add(this.textLvl);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Leveler";
+            this.Text = "Leveler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textLvl;
+        private System.Windows.Forms.TextBox textExp;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsForms/WindowsForms/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+
+    public partial class Leveler : Form
+    {
+        Player p = new Player();
+        public Leveler()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LvlUp();
+            display("textExp");
+        }
+
+        private void Progress(object sender, EventArgs e)
+        {
+
+            progressBar1.Value += 10;
+            p.EXP += progressBar1.Value;
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                progressBar1.Value = 0;
+                LvlUp();
+            }
+            display("textExp");
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (progressBar1.Value > 0)
+                progressBar1.Value -= 10;
+
+        }
+
+        private void textlvl_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textexp_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void SecretCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                p.LVL = 100;
+                p.EXP = 10000000;
+            }
+            display("textLvl");
+            display("textExp");
+            checkBox1.Checked = false;
+        }
+
+
+
+        void LvlUp()
+        {
+            display("textLvl");
+            p.LVL += 1;
+            progressBar1.Maximum = (p.LVL * 100) / 2;
+        }
+
+        public void display(string textfile)
+        {
+            if (textfile == "textLvl")
+                textLvl.Text = "LVL: " + p.LVL.ToString();
+            if (textfile == "textExp")
+                textExp.Text = "XP: " + p.EXP.ToString();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+            p.LVL = 1;
+            p.EXP = 0;
+
+            display("textLvl");
+            display("textExp");
+        }
+
+    }
+
+    public class Player
+    {
+        public Player()
+        {
+            exp = 0;
+            lvl = 1;
+        }
+        private int lvl;
+        private int exp;
+        public int EXP
+        {
+            get { return exp; }
+            set
+            {
+                if (value <= 0)
+                    exp = 0;
+                else
+                    exp = value;
+            }
+        }
+        public int LVL
+        {
+            get { return lvl; }
+            set
+            {
+                if (value <= 0)
+                    lvl = 0;
+                else
+                    lvl = value;
+            }
+        }
+    }
+}
